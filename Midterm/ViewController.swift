7cbc9dda84a9ff77f32193b5dffb35be216a645d
student <student@students-mac.student.georgianc.on.ca>
2016-02-26 10:25:51 -0500

    most of the constraints in place

--- Midterm/ViewController.swift
+++ Midterm/ViewController.swift
@@ -13,21 +13,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var item1Stepper: UIStepper!
     @IBOutlet weak var item1Quantity: UILabel!
     
-    @IBOutlet weak var item2Quantity: UILabel!
     @IBOutlet weak var item2Stepper: UIStepper!
+    @IBOutlet weak var item2Quantity: UILabel!
     
-    
+    @IBOutlet weak var item3Stepper: UIStepper!
     @IBOutlet weak var item3Quantity: UILabel!
     
-    
+    @IBOutlet weak var item4Stepper: UIStepper!
     @IBOutlet weak var item4Quantity: UILabel!
-    
-    
     
     @IBAction func item1StepperQuantityChanged(sender: UIStepper) {
         item1Quantity.text = Int(sender.value).description
     }
     
+    @IBAction func item2StepperQuantityChanged(sender: UIStepper) {
+    }
     
 
 }
